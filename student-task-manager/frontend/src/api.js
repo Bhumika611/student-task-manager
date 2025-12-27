d9d8af50e9031cefb4ce26f5aaa26d7fbcc1758c
Macharla Bhumika <bhumikamacharla@gmail.com>
2025-12-27 17:08:41 +0530

    Connect frontend to Render backend

--- student-task-manager/frontend/src/api.js
+++ student-task-manager/frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://student-task-backend-c5lx.onrender.com/api",
 });
 
 export default API;
